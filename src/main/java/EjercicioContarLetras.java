a9ae94a4c7c8ef55d70d01433f3664acad3018e4
Carlos <carlos@carloslandia.com>
2022-05-10 08:10:56 +0200

    Solución E2: Testing

--- src/main/java/EjercicioContarLetras.java
+++ src/main/java/EjercicioContarLetras.java
@@ -1,2 +1,34 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class EjercicioContarLetras {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        System.out.println(contarRepeticiones(input));
+
+    }
+
+    public static String contarRepeticiones(String text) {
+        if (text == null)
+            return null;
+
+        char[] arrayChar = text.toLowerCase().toCharArray();
+
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char character : arrayChar) {
+            if (map.containsKey(character)) {
+                Integer value = map.get(character);
+                value++;
+                map.replace(character, value);
+            } else {
+                map.put(character, 1);
+            }
+        }
+        return map.toString();
+
+    }
 }
--- src/test/java/TestEjercicioContarLetras.java
+++ src/test/java/TestEjercicioContarLetras.java
@@ -1,3 +1,53 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestEjercicioContarLetras {
+    @Test
+    public void testContarLetrasIguales(){
+        String input = "aaaa";
+        String outputEsperado = "{a=4}";
+        String outputActual = EjercicioContarLetras.contarRepeticiones(input);
+        Assertions.assertEquals(outputEsperado, outputActual);
+    }
+
+    @Test
+    public void testContarNumerosNoIguales(){
+        Integer[] input = {1,2,3,4};
+        String outputEsperado = "{1=1, 2=1, 3=1, 4=1}";
+        String outputActual = EjercicioContarNumeros.contarRepeticiones(input);
+        Assertions.assertEquals(outputEsperado, outputActual);
+    }
+
+    @Test
+    public void testContarNumerosMixtos(){
+        Integer[] input = {1,2,3,1};
+        String outputEsperado = "{1=2, 2=1, 3=1}";
+        String outputActual = EjercicioContarNumeros.contarRepeticiones(input);
+        Assertions.assertEquals(outputEsperado, outputActual);
+    }
+
+    @Test
+    public void testContarNumerosNegativos(){
+        Integer[] input = {-1,2,3,1};
+        String outputEsperado = "{-1=1, 1=1, 2=1, 3=2}";
+        String outputActual = EjercicioContarNumeros.contarRepeticiones(input);
+        Assertions.assertEquals(outputEsperado, outputActual);
+    }
+
+    @Test
+    public void testContarNumerosSinNumeros(){
+        Integer[] input = {};
+        String outputEsperado = "{}";
+        String outputActual = EjercicioContarNumeros.contarRepeticiones(input);
+        Assertions.assertEquals(outputEsperado, outputActual);
+    }
+
+    @Test
+    public void testContarNumerosConNull(){
+        Integer[] input = null;
+        String outputActual = EjercicioContarNumeros.contarRepeticiones(input);
+        Assertions.assertNull(outputActual);
+    }
+
 
 }
